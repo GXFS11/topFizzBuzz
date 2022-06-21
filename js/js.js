d3a09fdfb6e75e048eb867d0110e949dfca0b924
Alexis FS <0alexis0fs@gmail.com>
2022-06-21 09:48:10 -0500

    Redondea nums para funcionalidad

--- js/js.js
+++ js/js.js
@@ -2,16 +2,23 @@ const cmps = document.getElementsByClassName("calcN"),
 	ini = cmps[0], fin = cmps[1], fz = cmps[2], bz = cmps[3],
 	res = document.getElementsByClassName("desc")[0];
 function azar () {
-	ini.value = 1 + (mran(90));
-	fin.value = 10 + (mran(90));
-	fz.value = 2 + mran(10);
-	bz.value = 2 + mran(10);
+	ini.value = 1 + redo((mran(90)));
+	fin.value = 10 + redo((mran(90)));
+	fz.value = 2 + redo(mran(10));
+	bz.value = 2 + redo(mran(10));
 }
 function mran (max = 2) {
 	return Math.random () * max;
 }
+function redo (num) {
+	return Math.floor (num + 0.5);
+}
 function jugar () {
 	res.innerHTML = "";
+	ini.value = redo(ini.value);
+	fin.value = redo(fin.value);
+	fz.value = redo(fz.value);
+	bz.value = redo(bz.value);
 	let num = "", min = ini.value, max = fin.value;
 	if (fin.value < ini.value) { min = fin.value; max = ini.value; }
 	for (let i = min; i <= max; i ++) {
